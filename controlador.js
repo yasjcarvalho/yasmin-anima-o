01fe8482c5188a53163c1c79624a610b0b2cae4b
yasminjcarvalho <97072307+yasjcarvalho@users.noreply.github.com>
2022-12-06 16:22:21 -0300

    Initial commit

---
+++ controlador.js
@@ -0,0 +1,6 @@
+anime({
+    targets: '.planeta',
+    translateX: 400,
+    rotate: 360,
+    delay: anime.stagger(800, { start: 3000 }) // delay starts at 500ms then increase by 100ms for each elements.
+});
